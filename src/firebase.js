ec3d89b4bc80f643a4c62c986cb766cf66d8e789
unknown <kayodefredrickbalogun@gmail.com>
2021-06-06 03:20:39 +0100

    configs

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,15 @@ import firebase from "firebase"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD5KRZaV_h8h9Gh464kFNOm34V685EB3fw",
-//   authDomain: "facebook-backend-612d1.firebaseapp.com",
-//   projectId: "facebook-backend-612d1",
-//   storageBucket: "facebook-backend-612d1.appspot.com",
-//   messagingSenderId: "281818656641",
-//   appId: "1:281818656641:web:ab6d87c547305952294280",
-//   measurementId: "G-NKD4RJHMGF"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyD5KRZaV_h8h9Gh464kFNOm34V685EB3fw",
+  authDomain: "facebook-backend-612d1.firebaseapp.com",
+  projectId: "facebook-backend-612d1",
+  storageBucket: "facebook-backend-612d1.appspot.com",
+  messagingSenderId: "281818656641",
+  appId: "1:281818656641:web:ab6d87c547305952294280",
+  measurementId: "G-NKD4RJHMGF"
+};
 
 const firebaseConfig = {
   apiKey: "AIzaSyC3jZ2D41D4xA-5MtgNpJYBGh4vcFjjFGc",
